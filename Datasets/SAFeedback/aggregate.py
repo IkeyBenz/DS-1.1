53f4c02cef6d3f6d05c25aef4b813e1f21ce09cc
Ikey Benzaken <ikey.benz@gmail.com>
2019-02-13 16:14:25 -0800

    Addded more datasets and notebooks

---
+++ Datasets/SAFeedback/aggregate.py
@@ -0,0 +1,41 @@
+import pandas as pd
+import csv
+import os
+
+folders = ['./2016/', './2017/']
+files = [folder + f for folder in folders for f in os.listdir(folder)]
+
+def getResponses():
+    responses = {}
+    for f in files:
+        lines = list(csv.reader(open(f, 'r')))
+        questions = lines.pop(0)
+        for line in lines:
+            for index, field in enumerate(line):
+                if questions[index] not in responses:
+                    responses[questions[index]] = []
+                responses[questions[index]].append(field)
+    return responses
+
+def makeNewCSV():
+    responses = getResponses()
+    biggest = max([len(a) for a in responses.values()])
+    out = ','.join(responses.keys()) + '\n'
+    for i in range(biggest):
+        line = ''
+        for q in responses:
+            if i < len(responses[q]):
+                line += '"' + responses[q][i] + '"'
+            line += ','
+        out += line + '\n'
+    f = open('combined.csv', 'w')
+    f.write(out)
+    f.close()
+    
+
+makeNewCSV()
+
+
+
+            
+
